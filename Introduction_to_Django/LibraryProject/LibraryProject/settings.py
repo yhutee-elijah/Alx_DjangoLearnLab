0888a2717eae155f8e478ec9cb44fd603cbb88bc
Utibeabasi Elijah <utibeabasielijah11@gmail.com>
2025-03-05 16:50:37 +0100

    added django file

--- Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -37,12 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bookshelf'
+    'bookshelf',
+    'relationship_app',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
