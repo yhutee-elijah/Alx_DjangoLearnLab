ab78fed2e79122c561c25fdbf7b7bcff1679918f
Utibeabasi Elijah <utibeabasielijah11@gmail.com>
2025-03-06 19:20:52 +0100

    added django file

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('relationship_app.urls')),  # Include relationship_app's URLs
+]
+
