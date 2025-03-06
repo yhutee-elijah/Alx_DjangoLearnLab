7742c25d1cabb12621421c9dad21194f0b689544
Utibeabasi Elijah <utibeabasielijah11@gmail.com>
2025-03-06 18:17:12 +0100

    added django file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -11,5 +11,5 @@ from .models import Library
 # Class-based view to show details of a specific library
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
