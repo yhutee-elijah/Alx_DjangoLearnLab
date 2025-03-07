670a0be2763651c0602153c015453b7b0a13d904
Utibeabasi Elijah <utibeabasielijah11@gmail.com>
2025-03-07 11:34:51 +0100

    added django file

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,16 +1,6 @@
-from django.shortcuts import path
-from django.contrib.auth.views import LoginView, LogoutView 
-from django.contrib.auth.decorators import User_passes_test
-from . import views(
-    register, #explicitly import register view
-    list_books
-    LibraryDetailView,
-    user_login,
-    user_logout,
-    admin_view,
-    librarian_view,
-    member_view
-)
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import list_books, LibraryDetailView, register, user_login, user_logout
 
 def is_admin(user):
     return user.is_authenticated and user.userprofile.role == 'Admin'
@@ -18,13 +8,13 @@ def is_admin(user):
 def is_librarian(user):
     return user.is_authenticated and user.userprofile.role == 'Librarian'
 
-def is_member (user):
+def is_member(user):
     return user.is_authenticated and user.userprofile.role == 'Member'
 
 urlpatterns = [
-    path('register/', views.register, name='register'),  # User registration
+    path('register/', register, name='register'),  # User registration
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),  # Built-in login view
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),  # Built-in logout view
     path('books/', list_books, name='list_books'),  # Function-based view for books
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # Class-based view for libraries
-] 
+]
