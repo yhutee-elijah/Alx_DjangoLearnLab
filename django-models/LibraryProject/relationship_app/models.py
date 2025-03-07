6778beab302cc1fed63b5f39607a2f27a9229bcd
Utibeabasi Elijah <utibeabasielijah11@gmail.com>
2025-03-07 12:35:56 +0100

    added django file

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+    
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
@@ -26,3 +27,10 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
+    
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.user.username
