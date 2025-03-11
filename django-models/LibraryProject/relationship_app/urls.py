f3c065753f06d066e4947a8ddfb3f2a8296bf08c
Yhutee Elijah <Utibeelijah11@gmail.com>
2025-03-11 20:27:51 +0100

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import list_books, LibraryDetailView, register, user_login, user_logout
 
