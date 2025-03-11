c15e1d1d1181602b2848dd1c06fb70a51490111a
Yhutee Elijah <Utibeelijah11@gmail.com>
2025-03-11 21:44:46 +0100

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -18,4 +18,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),  # Built-in logout view
     path('books/', list_books, name='list_books'),  # Function-based view for books
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # Class-based view for libraries
+    path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),  # ✅ Admin-only view
 ]
+
+
