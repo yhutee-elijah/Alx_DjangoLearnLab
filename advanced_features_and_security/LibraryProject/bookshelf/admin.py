ac44c4fe107ead3296b8613e6f697633afa21cae
Utibeabasi Elijah <utibeabasielijah11@gmail.com>
2025-03-18 15:45:13 +0100

    added django file

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,10 +1,36 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 from .models import Book 
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')  # Display these fields in the admin list
     list_filter = ('publication_year', 'author')  # Add filtering options
-    search_fields = ('title', 'author')  # Enable searching by title and author             
+    search_fields = ('title', 'author')  # Enable searching by title and author     
+
+
+# Customize the admin panel for CustomUser
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    # Display these fields in the admin panel
+    list_display = ('username', 'email', 'date_of_birth', 'is_staff', 'is_superuser')
+
+    # Allow filtering by these fields
+    list_filter = ('is_staff', 'is_superuser')
+
+    # Customize the fieldsets to include additional fields
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+    # Customize the form for creating new users
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('username', 'email', 'password1', 'password2', 'date_of_birth', 'profile_photo')}),
+    )
+
+# Register the CustomUser model with CustomUserAdmin
+admin.site.register(CustomUser, CustomUserAdmin)            
 
 
