48e92b5714a3ea6550d49b00e6f63d723f342d6a
Utibeabasi Elijah <utibeabasielijah11@gmail.com>
2025-03-29 17:06:18 +0100

    added django file

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User  # Ensure this import is present
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)  # Title field
+    content = models.TextField()  # Content field
+    published_date = models.DateTimeField(auto_now_add=True)  # Published date field
+    author = models.ForeignKey(User, on_delete=models.CASCADE)  # Author field
+
+    def __str__(self):
+        return self.title  # Returns the title as a string representation
