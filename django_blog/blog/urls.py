3530c0b9ab769f5323ea0eb844eeff13562bef5c
Utibeabasi Elijah <utibeabasielijah11@gmail.com>
2025-03-29 22:11:13 +0100

    added django file

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -3,8 +3,8 @@ from django.contrib.auth.views import LoginView
 from .views import register_view, profile_view, profile_edit_view, CustomLogoutView
 from .views import (
     PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView,
+    CommentCreateView, CommentUpdateView, CommentDeleteView
 )
-from .views import add_comment, CommentUpdateView, CommentDeleteView, CommentCreateView
 
 urlpatterns = [
     path("login/", LoginView.as_view(template_name="blog/login.html"), name="login"),
@@ -17,9 +17,8 @@ urlpatterns = [
     path("post/new/", PostCreateView.as_view(), name="post-create"),
     path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
     path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
-    path("post/<int:post_id>/comments/new/", add_comment, name="add-comment"),
-    path("comment/<int:pk>/edit/", CommentUpdateView.as_view(), name="edit-comment"),
+    path("post/<int:pk>/comments/new/", CommentCreateView.as_view(), name="add-comment"),
+    path("comment/<int:pk>/update/", CommentUpdateView.as_view(), name="edit-comment"),
     path("comment/<int:pk>/delete/", CommentDeleteView.as_view(), name="delete-comment"),
-    path("post/<int:post_id>/comments/new/", CommentCreateView.as_view(), name="add-comment"),
 ]
 
