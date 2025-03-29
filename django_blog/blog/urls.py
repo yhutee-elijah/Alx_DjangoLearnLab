b23c0816a741655dfaae756cd4f811c754a8c34a
Utibeabasi Elijah <utibeabasielijah11@gmail.com>
2025-03-29 21:38:56 +0100

    added django file

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -12,9 +12,9 @@ urlpatterns = [
     path("profile/", profile_view, name="profile"),
     path("profile/edit/", profile_edit_view, name="profile_edit"),
     path("", PostListView.as_view(), name="post-list"),
-    path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
-    path("posts/new/", PostCreateView.as_view(), name="post-create"),
-    path("posts/<int:pk>/edit/", PostUpdateView.as_view(), name="post-update"),
-    path("posts/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("post/new/", PostCreateView.as_view(), name="post-create"),
+    path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
+    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
 ]
 
